4e15678f8b4eb1c4e272291781fe8d23a9914166
CesarRawr <rawrcesar13@gmail.com>
2023-05-02 15:20:24 -0600

    Adding getHostname and getMac function

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+  "os"
   "log"
   "fmt"
+  "net"
   "time"
+  "analyzer/models"
   "analyzer/controller"
   badger "github.com/dgraph-io/badger/v4"
 )
@@ -24,7 +27,48 @@ func show(db *badger.DB, id string) {
   fmt.Println(*oldLog)
 }
 
+func getHostname() (*string, error) {
+  hostname, err := os.Hostname()
+  if err != nil {
+    return nil, err
+  }
+
+  return &hostname, nil
+}
+
+func getMac() (string, error) {
+  ifas, err := net.Interfaces()
+  if err != nil {
+    return "", err
+  }
+
+  for _, ifa := range ifas {
+    a := ifa.HardwareAddr.String()
+    if a != "" {
+      return a, nil
+    }
+  }
+
+  return "", fmt.Errorf("could not find a valid network interface with a MAC address")
+}
+
 func main() {
+  hostname, err := getHostname();
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  mac, err := getMac()
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  fmt.Println(*hostname);
+  fmt.Println(mac);
+
+  initialLog := models.Log{}
+  fmt.Println(initialLog)
+
   db, err := badger.Open(badger.DefaultOptions("./.data"))
   if err != nil {
     log.Fatal(err)
