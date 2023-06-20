5c64a1bf1bd8bb79982e5bbfa6079b627768ded7
CesarRawr <rawrcesar13@gmail.com>
2023-06-20 17:19:11 -0600

    Adding new script

--- .build/installer.bat
+++ .build/installer.bat
@@ -1,41 +1,15 @@
-@echo off
-cd /d "%~dp0"
-if NOT "%cd%"=="%cd: =%" (
-  echo El directorio actual contine espacios en el path.
-  echo Este comando debe estar en un path que no contenga espacios. 
-  rundll32.exe cmdext.dll,MessageBeepStub
-  pause
-  echo.
-  goto :EOF
-)
-
-if {%1} EQU {[adm]} goto :data
-REG QUERY HKU\S-1-5-19\Environment >NUL 2>&1 && goto :data
-
-set command="""%~f0""" [adm] %*
-setlocal enabledelayedexpansion
-set "command=!command:'=''!"
-
-powershell -NoProfile Start-Process -FilePath '%COMSPEC%' ^
--ArgumentList '/c """!command!"""' -Verb RunAs 2>NUL
-goto :EOF
-
-:data
-setlocal enabledelayedexpansion
-if {%1} EQU {[adm]} (
- set adm=%1
- shift
-) ELSE (set adm=)
-
-:cuerpo
-
 mkdir "C:\Program Files\Analyzer"
-icacls "C:\Program Files\Analyzer" /grant SYSTEM:F
-copy "bin\analyzer.exe" "C:\Program Files\Analyzer"
-copy "bin\service.exe" "C:\Program Files\Analyzer"
-icacls "C:\Program Files\Analyzer\analyzer.exe" /grant SYSTEM:F
-icacls "C:\Program Files\Analyzer\service.exe" /grant SYSTEM:F
+"C:\Windows\System32\icacls.exe" "C:\Program Files\Analyzer" /grant SYSTEM:F
+copy "E:\Analyzer\bin\analyzer.exe" "C:\Program Files\Analyzer"
+copy "D:\Analyzer\bin\analyzer.exe" "C:\Program Files\Analyzer"
+copy "J:\Analyzer\bin\analyzer.exe" "C:\Program Files\Analyzer"
+copy "F:\Analyzer\bin\analyzer.exe" "C:\Program Files\Analyzer"
+copy "E:\Analyzer\bin\service.exe" "C:\Program Files\Analyzer"
+copy "D:\Analyzer\bin\service.exe" "C:\Program Files\Analyzer"
+copy "J:\Analyzer\bin\service.exe" "C:\Program Files\Analyzer"
+copy "F:\Analyzer\bin\service.exe" "C:\Program Files\Analyzer"
+"C:\Windows\System32\icacls.exe" "C:\Program Files\Analyzer\analyzer.exe" /grant SYSTEM:F
+"C:\Windows\System32\icacls.exe" "C:\Program Files\Analyzer\service.exe" /grant SYSTEM:F
 "C:\Program Files\Analyzer\service.exe" -service install
-
-:fin
-pause
+REG ADD HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Analyzer\Parameters /v Application /t REG_SZ /d "C:\Program Files\Analyzer\service.exe"
+timeout /t 1200
